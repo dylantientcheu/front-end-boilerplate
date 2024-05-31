145fc5d1106b8e148195c71a882a7b8c5d5298ff
Dylan Tientcheu <dylantientcheu@gmail.com>
2024-05-31 22:35:40 +0200

    init commit :tada:

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { Card } from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
 import Image from "next/image";
 
 export default function Home() {
@@ -11,13 +13,12 @@ export default function Home() {
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
             <Image
-              src="/vercel.svg"
+              src="/arey_light.png"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
@@ -29,14 +30,53 @@ export default function Home() {
       </div>
 
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <Card className="p-4">
+          <div className="flex flex-col space-y-8">
+            <div className="items-top flex space-x-2">
+              <Checkbox id="terms0" checked />
+              <div className="grid gap-1.5 leading-none">
+                <label
+                  htmlFor="terms0"
+                  className="text-sm flex space-x-1 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Clone the repository then npm install & npm run dev
+                </label>
+                <p className="text-xs text-muted-foreground">
+                  Get the repository and learn through the code.
+                </p>
+              </div>
+            </div>
+            <div className="items-top flex space-x-2">
+              <Checkbox id="terms1" />
+              <div className="grid gap-1.5 leading-none">
+                <label
+                  htmlFor="terms1"
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Copy environment variables from .env.example to .env
+                </label>
+                <p className="text-xs text-muted-foreground">
+                  Update your environment variables in the .env file.
+                </p>
+              </div>
+            </div>
+            <div className="items-top flex space-x-2">
+              <Checkbox id="terms2" />
+              <div className="grid gap-1.5 leading-none">
+                <label
+                  htmlFor="terms2"
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Go on and have fun!
+                </label>
+                <p className="text-xs text-muted-foreground">
+                  No stress, Take your time to show us what you can do.
+                </p>
+              </div>
+            </div>
+          </div>
+        </Card>
+
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
